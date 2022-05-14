0ab6a9512b5a4010c6a15e32f39609e2026dc2cf
RxiPland <82058894+RxiPland@users.noreply.github.com>
2022-05-14 17:06:23 +0200

    Add files via upload

--- adelka.py
+++ adelka.py
@@ -17,8 +17,11 @@ def main():
     for video in videos:
 
         break
-
-    video = str(video['videoId'])
+        
+    video_id = str(video['videoId'])
+    nazev_videa = str(video['title']['runs'][0]['text'])
+    vydani_videa_cas = str(video['publishedTimeText']['simpleText'])
+    pocet_zhlednuti = str(video['viewCountText']['simpleText'])
 
     try:
 
@@ -31,21 +34,27 @@ def main():
 
         posledni = ""
 
-    if posledni == video:
+    if posledni == video_id:
 
-        url = "https://www.youtube.com/watch?v=" + video + posledni
+        url = "https://www.youtube.com/watch?v=" + video_id + posledni
 
-        print("Žádné nové video.\n\nStaré: " + url)
+        print("Žádné nové video.\n\nStaré: " + url + "\n")
 
     else:
 
         f = open("posledni_video.txt", "wb")
-        f.write(video.encode("UTF-8"))
+        f.write(video_id.encode("UTF-8"))
         f.close()
 
-        url = "https://www.youtube.com/watch?v=" + video
+        url = "https://www.youtube.com/watch?v=" + video_id
 
-        print("Nové video!\n\n" + url)
+        print("Nové video!\n\nOdkaz: " + url + "\n")
+
+    print("Název:", nazev_videa)
+    print("Vydáno:", vydani_videa_cas)
+    print("Počet zhlédnutí:", pocet_zhlednuti)
+
+    print()
 
 
     konec = input("\n[K]opírovat URL\n[O]tevřít v prohlížeči\n[E]xit\n\n").upper()
@@ -70,3 +79,6 @@ def main():
         pass
 
 main()
+
+
+
